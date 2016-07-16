a94342310b8afc67c2ed25fd6ca483f6d649a2da
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-15 23:06:16 -0500

    Refactored settings editor

--- index.php
+++ index.php
@@ -11,25 +11,26 @@ include "../config.php";
 include "../includes/bootstrap.inc";
 if( ! $account->_is_admin ) throw_fake_404();
 
-$messages = $errors = array();
-if( $_REQUEST["mode"] == "save_group" )
+if( ! empty($_POST["names"]) )
 {
+    $messages = array();
+    
     foreach($_POST["names"] as $key => $val)
     {
         $val = trim(stripslashes($val));
         list($group, $var) = explode(".", $key);
-        if( empty($val) )
+        if( $val != $settings->get($key) )
         {
-            # $errors[] = replace_escaped_vars($current_module->language->admin->record_nav->errors->empty_val, '{$name}', ucwords(str_replace("_", " ", $var)));
+            $settings->set($key, $val);
+            $messages[] = replace_escaped_vars($current_module->language->admin->record_nav->errors->var_ok, '{$name}', ucwords(str_replace("_", " ", $var)));
         }
-        else
-        {
-            if( $val != $settings->get($key) )
-            {
-                $settings->set($key, $val);
-                $messages[] = replace_escaped_vars($current_module->language->admin->record_nav->errors->var_ok, '{$name}', ucwords(str_replace("_", " ", $var)));
-            }
-        }
+    }
+    
+    if( count($messages) > 0 )
+    {
+        send_notification($account->id_account, "success", implode("\n", $messages));
+        
+        die("OK");
     }
 }
 
